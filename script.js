17abd1be796d7b5c30efd1d9f277ca474886bd09
Code-Creator-man <justforcodings@hotmail.com>
2025-05-26 21:46:04 +0500

    Final start and bugs are fixed mispelled articles are also fixed

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
  tailwind.config = {
         theme: {
           extend: {
-            colors: { primary: "#6366F1", secondary: "#8B5CF6" },
+            colors: { primary: "#8B5CF6", secondary: "#EC4899" },
             borderRadius: {
               none: "0px",
-              sm: "4px",
-              DEFAULT: "8px",
-              md: "12px",
-              lg: "16px",
-              xl: "20px",
-              "2xl": "24px",
+              sm: "8px",
+              DEFAULT: "12px",
+              md: "16px",
+              lg: "20px",
+              xl: "24px",
+              "2xl": "28px",
               "3xl": "32px",
               full: "9999px",
-              button: "8px",
+              button: "12px",
             },
           },
         },
