46ae0941aefe1bcd9d315ca10922eba607a63e1b
Code-Creator-man <justforcodings@hotmail.com>
2025-06-03 15:31:21 +0500

    Project details

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-fetch(" https://pokeapi.co/api/v2/pokemon/pikachu")
-.then
+const fetchNews = async (page) => {
+    console.log("Fetching page...")
+    var url = 'https://newsapi.org/v2/everything?'+
+    'q=Apple&' +
+    'from=2025=05-20&' +
+    'pageSiz=20&' +
+    'page=' +page+
+    'sortBy=popularity&' +
+    'apiKey='
+}
